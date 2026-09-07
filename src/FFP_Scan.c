3a5bfc252712c3e828aca84cd1e53a1cb7c35506
GpapPeaky <giorgospapamat@gmail.com>
2026-09-07 06:21:07 +0300

    FInished with format checks, field checks and value return, need to optimize, then restructure files

--- src/FFP_Scan.c
+++ src/FFP_Scan.c
@@ -6,39 +6,230 @@ static int ffp_validate_line_format(const char* line) {
     // field=value   -> integer
     // field="value" -> string
 
-    fprintf(stderr, "[FFP_ERR] INVALID LINE FORMAT\n");
-
-    char* tokens[3];
-
     char* trimmed = trim_outer_string(line);
-    
-    int opVisited  = 0;
-    int valVisited = 0;
-    
-    printf("original line %s\n", line);
-    printf("trimmed line %s\n", trimmed);
-    
-    char* pch = strtok(trimmed, "="); /* Seperate from '=' */
 
-    tokens[0] = strdup(pch); /* Copy the variable */
-    
-    if (pch) opVisited = 1;
-    
-    unsigned char count = 0 ;
-    while (pch != NULL && count < 3) {
-        printf("token %s\n", pch);
-        tokens[count++] = strtok(NULL, " "); /* Seperate from spaces */
+    // printf("LINE %s\n", line);
+    // printf("TRIMMED %s\n", trimmed);
+
+    char* compressed = malloc(sizeof(char) * 512);
+
+    size_t idx = 0;
+    size_t whitespace_count = 0;
+    while (!is_null(trimmed[idx])) {
+        if (trimmed[idx] == ' ') {
+            whitespace_count++;
+            
+            idx++;
+
+            continue; // Skip whitespace
+        }
+        
+        compressed[idx - whitespace_count] = trimmed[idx];
+
+        idx++;
     }
-    
-    return opVisited && valVisited;
+
+    idx = 0;
+
+    // printf("COMPRESSED %s\n", compressed);
+
+    typedef enum {
+        FIELD = 0,
+        OP,
+        VAL
+    } line_format;
+
+    // Save tokens here, validate later
+    char* tokens[3];
+    for (int j = 0 ; j < 3 ; j++) {
+        tokens[j] = malloc(sizeof(char) * 512);
+        // tokens[j] = NULL;
+    }
+
+    int opFound = 0;
+    int opFoundIdx = 0;
+    while (!is_null(compressed[idx])) {
+        if (compressed[idx] != '=' && !opFound) {
+            tokens[FIELD][idx] = compressed[idx];
+
+            idx++;
+
+            continue;
+        } else if (compressed[idx] != '=' && opFound) {
+            tokens[VAL][idx - opFoundIdx - 1] = compressed[idx];
+
+            idx++;
+
+            continue;
+        } else if (compressed[idx] == '=') {
+            opFound = 1;
+            opFoundIdx = idx++;
+
+            tokens[OP][0] = '=';
+
+            continue;
+        } else {
+            return 0;
+        }
+    }
+
+    // printf("%s\n", tokens[FIELD]);
+    // printf("%s\n", tokens[OP]);
+    // printf("%s\n", tokens[VAL]);
+
+    return tokens[FIELD] && tokens[OP] && tokens[VAL];
 } 
 
 static char* ffp_get_field_from_line(const char* line) {
+    // Format:
+    //
+    // field=value   -> integer
+    // field="value" -> string
+
+    char* trimmed = trim_outer_string(line);
+
+    // printf("LINE %s\n", line);
+    // printf("TRIMMED %s\n", trimmed);
+
+    char* compressed = malloc(sizeof(char) * 512);
+
+    size_t idx = 0;
+    size_t whitespace_count = 0;
+    while (!is_null(trimmed[idx])) {
+        if (trimmed[idx] == ' ') {
+            whitespace_count++;
+            
+            idx++;
+
+            continue; // Skip whitespace
+        }
         
-    
+        compressed[idx - whitespace_count] = trimmed[idx];
 
+        idx++;
+    }
 
-    return "x";
+    idx = 0;
+
+    // printf("COMPRESSED %s\n", compressed);
+
+    typedef enum {
+        FIELD = 0,
+        OP,
+        VAL
+    } line_format;
+
+    // Save tokens here, validate later
+    char* tokens[3];
+    for (int j = 0 ; j < 3 ; j++) {
+        tokens[j] = malloc(sizeof(char) * 512);
+        // tokens[j] = NULL;
+    }
+
+    int opFound = 0;
+    int opFoundIdx = 0;
+    while (!is_null(compressed[idx])) {
+        if (compressed[idx] != '=' && !opFound) {
+            tokens[FIELD][idx] = compressed[idx];
+
+            idx++;
+
+            continue;
+        } else if (compressed[idx] != '=' && opFound) {
+            tokens[VAL][idx - opFoundIdx - 1] = compressed[idx];
+
+            idx++;
+
+            continue;
+        } else if (compressed[idx] == '=') {
+            opFound = 1;
+            opFoundIdx = idx++;
+
+            tokens[OP][0] = '=';
+
+            continue;
+        } else {
+            return 0;
+        }
+    }
+
+    return tokens[FIELD];
+}
+
+static char* ffp_get_value_from_line(const char* line) {
+    // Format:
+    //
+    // field=value   -> integer
+    // field="value" -> string
+
+    char* trimmed = trim_outer_string(line);
+
+    // printf("LINE %s\n", line);
+    // printf("TRIMMED %s\n", trimmed);
+
+    char* compressed = malloc(sizeof(char) * 512);
+
+    size_t idx = 0;
+    size_t whitespace_count = 0;
+    while (!is_null(trimmed[idx])) {
+        if (trimmed[idx] == ' ') {
+            whitespace_count++;
+            
+            idx++;
+
+            continue; // Skip whitespace
+        }
+        
+        compressed[idx - whitespace_count] = trimmed[idx];
+
+        idx++;
+    }
+
+    idx = 0;
+
+    // printf("COMPRESSED %s\n", compressed);
+
+    typedef enum {
+        FIELD = 0,
+        OP,
+        VAL
+    } line_format;
+
+    // Save tokens here, validate later
+    char* tokens[3];
+    for (int j = 0 ; j < 3 ; j++) {
+        tokens[j] = malloc(sizeof(char) * 512);
+        // tokens[j] = NULL;
+    }
+
+    int opFound = 0;
+    int opFoundIdx = 0;
+    while (!is_null(compressed[idx])) {
+        if (compressed[idx] != '=' && !opFound) {
+            tokens[FIELD][idx] = compressed[idx];
+
+            idx++;
+
+            continue;
+        } else if (compressed[idx] != '=' && opFound) {
+            tokens[VAL][idx - opFoundIdx - 1] = compressed[idx];
+
+            idx++;
+
+            continue;
+        } else if (compressed[idx] == '=') {
+            opFound = 1;
+            opFoundIdx = idx++;
+
+            tokens[OP][0] = '=';
+
+            continue;
+        } else {
+            return 0;
+        }
+    }
+
+    return tokens[VAL];
 }
 
 void* ffp_read_value(const char* value_string) {
@@ -56,23 +247,21 @@ void* ffp_read_value(const char* value_string) {
     long long unsigned int lineNum = 1; 
 
     while (fgets(line, sizeof(line), ffp_file) != NULL) {
-        char *fieldLine = trim_outer_string(line);
-        
-        if (!fieldLine) continue;
+        if (!line) continue;
 
-        int valid = ffp_validate_line_format(fieldLine);
+        int valid = ffp_validate_line_format(line);
 
         if (!valid) {
             fprintf(stderr,"Invalid line format at line %d\n", lineNum);
             continue;
         }
 
-        char* field = ffp_get_field_from_line(fieldLine);
+        char* field = ffp_get_field_from_line(line);
 
         if (str_compare(value_string, field)) {
-            printf("Field \'%s\', found\n", field);
+            // printf("Field \'%s\', found\n", field);
 
-            return field;
+            return ffp_get_value_from_line(line);
         }
 
         lineNum++;
