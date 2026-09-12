efeda749a1d87fd2a563a56a093a9e8a0181cb4c
GpapPeaky <giorgospapamat@gmail.com>
2026-09-12 13:49:53 +0300

    MInor testing for reading integers as integers and not only as strings

--- examples/FlexibleFileParser.c
+++ examples/FlexibleFileParser.c
@@ -7,9 +7,13 @@ int main(void) {
     ffp_bind_file(f);
 
     char* p = ffp_read_value("x");
+    int* p2 = ffp_read_value("val2");
 
     if(p)
         printf("%s\n", p);
+
+    if(p2)
+        printf("%s\n", p2);
 
     return 0;
 }
